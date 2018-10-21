86ad0c66b93b153b6e9143cf2321e2220fe92ed9
zongyc <zongyc@uw.edu>
2018-10-20 17:23:21 -0700

    start of Backend

---
+++ Backend/Util.go
@@ -0,0 +1,8 @@
+package Backend
+
+const distanceThreshold float64 = 1609.34 / 2; // half a mile
+
+func Close(longitude1 float64, latitude1 float64, longitude2 float64, latitude float64) bool {
+	r := 6378.137;
+	latDiff :=;
+}
